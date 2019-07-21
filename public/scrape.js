7c2ffdea4571e0fbcf45ab29d6b852505303b0a5
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-07-21 11:25:54 -0600

    get articles route complete

--- public/scrape.js
+++ public/scrape.js
@@ -1,16 +1,19 @@
 
 //Handles ajax call for usa today news scrape
-$("#news-scrape").on("click", function() {
+$(document).on("click", "#news-scrape", function() {
     $.get("/scrape", function(data) {
         generateModal(data);
     });
 });
 
+//generates modal and option to retrieve articles from mongoDB
 function generateModal(data) {
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
 
     modal.style.display = "block";
+
+    $(".modal-text").html("");
 
     let modalDiv = $("<div>");
 
@@ -48,13 +51,42 @@ function generateModal(data) {
     }
 }
 
+//Initializes ajax call and retrieves articles from mongoDB
 $(document).on("click", "#view-news", function() {
-    console.log("testeroo");
+    
     var modal = document.getElementById("myModal");
 
     modal.style.display = "none";
 
     $.getJSON("/news", function(data) {
-        console.log(data);
+        generateNews(data);
     });
 });
+
+//Renders articles to page
+function generateNews(data) {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        
+        let savedArticle = data[i];
+
+        let newsDiv = $("<div>");
+        newsDiv.addClass("col-md-12 pt-3");
+
+        let newsTitle = $("<h1>");
+        newsTitle.text(savedArticle.title);
+        newsDiv.append(newsTitle);
+
+        let newsSummary = $("<h5>");
+        newsSummary.text(savedArticle.summary);
+        newsDiv.append(newsTitle);
+
+        let newsLink = $("<a>");
+        newsLink.attr("href", "https://news.google.com" + savedArticle.link);
+        newsLink.html(savedArticle.link);
+        newsDiv.append(newsLink);
+
+        $("#news").prepend(newsDiv);
+
+    }
+}
