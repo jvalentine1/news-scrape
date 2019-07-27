d069e157d0fce013b5d6f3f422101d592b69fe09
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-07-27 09:50:10 -0600

    port update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 
 // models and port
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
